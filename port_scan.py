3e6035d67607503ebcbcf834caf848ae2ac0aa03
jwarlow <jmooney9001@gmail.com>
2017-09-11 17:00:39 -0400

    pyPort - finished typing out port scanner; begin debug

--- port_scan.py
+++ port_scan.py
@@ -12,5 +12,22 @@ https://stackoverflow.com/questions/12090503/listing-available-com-ports-with-py
 	if sys.platform.startswith('win'):
 		ports = ['COM%s' % (i + 1) for i in range(256)]
 	elif sys.platform.startswith('linux') or sys.platform.startwith('cygwin'):
-	# apparently excludes "/dev/tty"
-	ports = glob.glob('/dev/tty[A-Za-z]*')
+		# apparently excludes "/dev/tty"
+		ports = glob.glob('/dev/tty[A-Za-z]*')
+	elif sys.platform.startswith('darwin'):
+		ports = glob.glob('/dev/tty.*/)
+	else:
+		raise EnvironmentError('Unsupported platform')
+		
+	reult = []
+	for port in ports:
+		try:
+			s. = serial.Serial(port)
+			s.close()
+			result.append(port)
+		except (OSError, serial.SerialException):
+			pass
+	return result
+	
+if __name__ == '__main__':
+	print(serial_ports())
